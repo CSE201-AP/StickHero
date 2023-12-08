ed93ecc43f8231700ed9e116cc4dcf2ec5ecf2b7
Ankur Bohra <ankurbohra04@gmail.com>
2023-12-08 16:01:12 +0530

    Add collision explanation

--- src/main/java/com/example/stickhero/CollisionTimer.java
+++ src/main/java/com/example/stickhero/CollisionTimer.java
@@ -7,10 +7,17 @@ import com.example.stickhero.Callback;
 
 import java.util.Map;
 
+/**
+ * Checks for collisions every frame between the given node and all "obstacle"
+ * nodes provided via callbacks.
+ * If the source node is also present in the provided obstacles, the
+ * corresponding callback is invoked when there are no obstacle collisions.
+ */
 public class CollisionTimer extends AnimationTimer {
     private final Map<Node, Callback> callbacks;
     private boolean colliding = false;
     private final Node node;
+
     public CollisionTimer(Node node, Map<Node, Callback> callbacks) {
         super();
         this.node = node;
@@ -20,11 +27,10 @@ public class CollisionTimer extends AnimationTimer {
     @Override
     public void handle(long l) {
         colliding = false;
-        callbacks.forEach((Node otherNode, Callback callback) -> {
-            if (otherNode == node) return;
-            Bounds otherNodeInScene = otherNode.localToScene(otherNode.getBoundsInLocal());
+        callbacks.forEach((Node obstacle, Callback callback) -> {
+            if (obstacle == node) return;
+            Bounds otherNodeInScene = obstacle.localToScene(obstacle.getBoundsInLocal());
             if (node.intersects(node.sceneToLocal(otherNodeInScene))) {
-                System.out.println(node + " intersecting with " + otherNode);
                 this.colliding = true;
                 callback.function();
             }
